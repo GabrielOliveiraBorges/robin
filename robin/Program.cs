7deab7e73b35558c3e9adb5d1aef57996ae117bc
Gabriel <Gabriel@DESKTOP-OFSS190>
2025-03-26 21:18:08 -0300

    Add project files.

---
+++ robin/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio_1
+{
+    class Aluno
+    {
+        public int RA;
+        public string Nome;
+        public double NotaProva;
+        public double NotaTrabalho;
+        public double NotaFinal;
+        public void CalcularMedia()
+        {
+            NotaFinal = (NotaProva + NotaTrabalho) / 2;
+        }
+
+        public bool CalcularNotaFinal()
+        {
+            double notaNecessaria = 6 - NotaFinal;
+            Console.WriteLine($"Nota que precisa para prova final: {notaNecessaria}");
+            return NotaFinal >= 6;
+        }
+
+        public void ImprimirNotaFinal()
+        {
+            Console.WriteLine($"Nota final {Nome}: {NotaFinal}");
+        }
+
+        public void ReceberDados()
+        {
+            Console.Write("RA: ");
+            RA = int.Parse(Console.ReadLine());
+            Console.Write("Nome: ");
+            Nome = Console.ReadLine();
+            Console.Write("Nota: ");
+            NotaProva = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Nota do trabalho : ");
+            NotaTrabalho = Convert.ToDouble(Console.ReadLine());
+        }
+    }
+
+}
